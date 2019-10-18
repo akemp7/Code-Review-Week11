3a7140aa5c3c4ddd7cb54bfa778ae35a80fefb6a
Anita Kemp <anita.k.kemp@gmail.com>
2019-10-18 11:19:08 -0700

    Add home controller route

---
+++ HairSalon.Solution/HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+    
+}
